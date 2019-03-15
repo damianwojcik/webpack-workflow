2b510841e759c02cc7a73c4a824499c97fb1a6f7
Damian Wójcik <damian.wojcik@agora.pl>
2019-03-15 19:06:25 +0100

    first commit

---
+++ src/containers/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'React';
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome Users</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
